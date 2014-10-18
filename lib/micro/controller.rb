2b6f7846446418dfac630fad6c811284c5f72e53
Ashley Towns <ashleyis@me.com>
2014-10-18 20:57:13 +1030

    layout controller directive for custom layouts

--- lib/micro/controller.rb
+++ lib/micro/controller.rb
@@ -5,6 +5,16 @@ module Micro
     include Micro::Routing
     include Micro::Helpers
 
+    class << self
+      def layout(name=nil)
+        if name.nil?
+          @layout ||= 'default'
+        else
+          @layout = name
+        end
+      end
+    end
+
     def initialize(info)
       @info = info
     end
@@ -12,8 +22,8 @@ module Micro
     private
 
     def layout(content)
-      view_name = File.join(Micro::Config.shared.layouts_path, 'default')
-      view_file = Micro::View.find(view_name)
+      view_name = File.join(Micro::Config.shared.layouts_path, self.class.layout)
+      view_file = Micro::View.find_view(view_name)
       if view_file.nil?
         content
       else
@@ -24,7 +34,9 @@ module Micro
     def render(str)
       view_folder = @info[:file].gsub(/_controller$/, '')
       view_name = File.join(Micro::Config.shared.views_path, view_folder, @info[:action].to_s)
-      [200, {'Content-Type' => 'text/html'}, [layout(Micro::View.new(view_name, self).render)]]
+      rendered = layout(Micro::View.new(view_name, self).render)
+
+      [200, {'Content-Type' => 'text/html'}, [rendered]]
     end
   end
 end
