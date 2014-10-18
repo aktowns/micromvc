be78a8c91579e8aa5ebf50e2e736231e295e1fbd
Ashley Towns <ashleyis@me.com>
2014-10-19 04:32:20 +1100

    added support for regex in routes, passing arguments to blocks.

--- lib/micro/support/router.rb
+++ lib/micro/support/router.rb
@@ -16,15 +16,22 @@ module Micro::Support
       path = env['PATH_INFO']
       method = env['REQUEST_METHOD'].downcase.to_sym
       route = @routes[path]
+      args = []
       if route.nil?
-        static_404 = File.join(Micro::Config.shared.gem_static, '404.erb')
-        raise HttpException.new(404, Tilt.new(static_404).render(self, path: path, method: method))
+        rgx = @routes.keys.select{|x|Regexp.new("^#{x}$") =~ path}
+        if rgx.length > 0
+          route = @routes[rgx.first]
+          args  = [Regexp.new("^#{rgx.first}").match(path)[1]]
+        else
+          static_404 = File.join(Micro::Config.shared.gem_static, '404.erb')
+          raise HttpException.new(404, Tilt.new(static_404).render(self, path: path, method: method))
+        end
       end
       target = route[method.to_sym]
-
       controller = target[:controller].new(target)
       controller.initialize_helpers
-      res = controller.instance_eval(&target[:block])
+
+      res = controller.instance_exec(*args, &target[:block])
       res.class == Array ? res : controller.render
     rescue HttpException => e
       [e.status, {}, [e.message]]
